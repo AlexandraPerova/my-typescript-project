fc0d2670dbd07ecadeb05518bdd6d6ac9ff7b632
aperova <Sasha.ava@mail.ru>
2024-07-06 20:48:51 +0300

    Changed html file

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,14 @@
-var character = 'mario';
+"use strict";
+const character = 'mario';
 console.log(character);
-var inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll('input');
 console.log(inputs);
-inputs.forEach(function (input) {
+inputs.forEach(input => {
     console.log(input);
 });
+let n = 5;
+console.log(n);
+n = 56;
+const b1 = true;
+console.log(b1, typeof b1);
+//# sourceMappingURL=sandbox.js.map
--- sandbox.ts
+++ sandbox.ts
@@ -9,3 +9,10 @@ console.log(inputs);
   inputs.forEach(input => {
     console.log(input)
 });
+
+  let n: number = 5;
+  console.log(n);
+  n = 56;
+
+  const b1: boolean = true;
+  console.log(b1, typeof b1);
